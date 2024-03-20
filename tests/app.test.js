7be570bfd69fb04f16ac077c40ec15d2cf2eeb33
Danil <dg444k@gmail.com>
2024-03-21 01:51:31 +0300

    -reformat function add on subtraction

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
-const sum = require('../app')
+const subtraction = require('../app')
 
-test('adds 1 + 2 to equal 3', () => {
-	expect(sum(1, 2)).toBe(3);
+test('subtraction 3 - 2 to equal 1', () => {
+	expect(subtraction(3, 2)).toBe(1);
 })
