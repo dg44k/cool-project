824fd3fecf3cc9026dce72d66947449d3c94f303
Danil <90725810+dg44k@users.noreply.github.com>
2024-04-14 12:40:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const port = 3001
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
 
 function subtraction(a, b) {
