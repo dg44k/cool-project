8e407df8b88649dc0240540d4e24377e0b104ab2
Danil <dg444k@gmail.com>
2024-05-11 04:17:40 +0300

    asda

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path')
 const app = express()
 const port = 3001
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
